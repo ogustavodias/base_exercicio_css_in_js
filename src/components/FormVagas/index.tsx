a8c3c57caf27b17d3c02594d90b85d6b38dad118
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-02-13 22:31:49 -0300

    styled-components aplicado ao FormVagas

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import Component from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,17 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
+    <Component onSubmit={aoEnviarForm}>
       <input
-        className={styles.campo}
+        className="campo"
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
+      <button className="btnPesquisar" type="submit">
         Pesquisar
       </button>
-    </form>
+    </Component>
   )
 }
 export default FormVagas
