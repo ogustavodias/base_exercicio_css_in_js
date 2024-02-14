6c24cc04508a3203fc7a43b46218d9cd08593264
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-02-13 22:32:44 -0300

    Pequena correção da classe heroTitle no Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,7 +3,9 @@ import Component from './styles'
 const Hero = () => (
   <Component>
     <div className="container">
-      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
+      <h2 className="heroTitle">
+        As melhores vagas para tecnologia, design e artes visuais.
+      </h2>
     </div>
   </Component>
 )
